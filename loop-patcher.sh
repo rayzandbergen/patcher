0a7d39f753f5457712c5984a3d59ea38b7fe7fb9
Ray Zandbergen <ray.zandbergen@gmail.com>
2013-11-06 18:56:58 +0000

    Only pause before restart after an immediate crash, otherwise restart as quickly a possible.
    git-svn-id: svn://localhost/home/ray/svnrepos/patcher@25 7c8a5990-47a6-46bc-89a8-0082861630d0

--- loop-patcher.sh
+++ loop-patcher.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 echo $BASHPID > /tmp/patcher.pid
 while true; do
+    t1=`date +%s`
     ./patcher
-    sleep 1;
+    t2=`date +%s`
+    if [ `expr $t2 - $t1` -lt 3 ]; then
+        # immediate crash, wait
+        sleep 1;
+    fi
 done
