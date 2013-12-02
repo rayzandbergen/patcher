e35f8e82b7f33a4431e6c0d29d2929f8b205f8f6
Ray Zandbergen <ray.zandbergen@gmail.com>
2013-12-02 20:12:16 +0000

    Added linkable svg's.
    git-svn-id: svn://localhost/home/ray/svnrepos/patcher@189 7c8a5990-47a6-46bc-89a8-0082861630d0

--- src/patchercore.h
+++ src/patchercore.h
@@ -9,9 +9,13 @@
 
 This MIDI patcher is an application that is intended to run on a Raspberry Pi board.
 It was implemented because my off-the-shelf MIDI controllers are not powerful enough
-to manage all the different sound settings and keyboard layouts that I use.
+to manage all the different sound settings and keyboard layouts that I use in my setup.
 
-\image html midi-setup.svg
+\htmlonly
+<div align='center'>
+<embed src="midi-setup.svg" type="image/svg+xml"/>
+</div>
+\endhtmlonly
 
 When playing a single track of the Noble Silence set list, a single Fantom Performance used.
 A Performance is a full mix of at most 16 sounds plus a chain of effects.
@@ -21,12 +25,18 @@ In my setup, switching is controlled by an FCB1010 MIDI foot controller.
 
 The goal of this MIDI patcher implementation is to create and manage keyboard layouts in live situations.
 
-\section rolandSpeak Roland-speak
+\section rolandSpeak Roland-speak: Performances, Parts, and Patches.
 
 A Fantom perfomance contains 16 'parts'.
 A part points to a 'patch', i.e. the actual sound, and a lot of parameters that determine how this sound is mixed, e.g. volume, pan and various effect chains.
 In addition, a part determines the MIDI channel for a sound, a key range within this channel, an optional transposition, and some high level parameters to modify the patch.
-\image html fantom.svg
+
+\htmlonly
+<div align='center'>
+<embed src="fantom.svg" type="image/svg+xml"/>
+</div>
+\endhtmlonly
+
 By convention, there is a single part listening on each MIDI channel, so Fantom part 1 listens of MIDI channel 1, and so on.
 However, this application will handle multiple parts per channel correctly.
 The one-part-for-each-channel convention makes it easier for humans to keep an overview of a track definitions, at the expense of slightly more MIDI traffic in case of patch layering.
@@ -39,24 +49,29 @@ Indeed, this application will not start up correctly with the Fantom in patch mo
 
 \section tracksectionpart Tracks, Sections, and Parts
 
+The patcher replaces Roland-speak with different abstractions.
+
 A track is usually a single item of the Noble Silence setlist.
 It maps naturally onto a Fantom Performance.
 
-Within a track there are sections. A section is a single keyboard layout.
+Within a track there are sections.
+A section is a single keyboard layout.
+This is the most important abstraction that is introduced by the patcher.
+It frees the Fantom from having to manage splits and layers.
 
 Finally, a section consists of parts. A part controls a single sound in the track mix. Parts are used to create keyboard splits (by limiting the key range of a part) and layers (by having overlapping key ranges for multiple parts). Parts in the patcher are roughly equivalent to parts in the Fantom. They are called \a SwPart to distinguish them from \a Fantom::Part.
 
 \section metaMode Meta mode
 
 In meta mode, keys on the A30 keyboard behave like buttons that control the patcher's behaviour.
+In live situations, this is the mode that is used to select the next track from the setlist.
 
 Meta mode is enabled by pressing the 'reverb' switch on the A30, or by pressing pedal 7 on the FCB1010.
 Is is disabled by pressing the 'reverb' button again, or by pressing pedal 6.
-- C4 Selects previous track in set list.
-- D4 Selects next track in set list.
+- C4 Selects previous track in the set list.
+- D4 Selects next track in the set list.
 - E4 Toggles info mode, where the Fantom display is used to show the network interfaces of the Pi.
 - C5 and up select a track directly.
-
 
 \section sectionSwitching Switching between sections
 
@@ -65,6 +80,8 @@ Section switches are triggered by the FCB foot controller.
 - If chaining is enabled for the current track (in "tracks.xml"), then pressing
 pedal 1 or 2 selects the previous section, while pedals 3 to 5
 select the next section.
+It is possible to link to a section in a different track.
+The correct performance will be selected automatically.
 - If chaining is disabled, then pedals 1 to 5 select a section directly.
 
 \section trackSwitching Switching between tracks
@@ -74,6 +91,21 @@ Switching between tracks is done in meta mode.
 \section configuration Configuration
 
 Configuration is read from an XML file. A schema XSD is provided.
+
+\section processes Processes
+The application consists of 3 processes.
+- The patcher core, which reads and writes MIDI data, and generates events.
+- The curses client, which produces terminal screen output.
+- The patcher administrator, which creates and cleans up the two processes above, as well as the event queue.
+
+\htmlonly
+<div align='center'>
+<embed src="processes.svg" type="image/svg+xml"/>
+</div>
+\endhtmlonly
+
+This configuration decouples the MIDI event loop in the core from the screen update loop.
+Since screen updates are less important than the responsiveness of the core, it is given a lower scheduling priority.
 
  */
 #ifndef PATCHER_H
