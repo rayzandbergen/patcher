80f55c5bf0d00752566c8d199cc0dc3f362be953
Ray Zandbergen <ray.zandbergen@gmail.com>
2013-12-18 19:57:17 +0000

    Added stdoutclient.
    git-svn-id: svn://localhost/home/ray/svnrepos/patcher@217 7c8a5990-47a6-46bc-89a8-0082861630d0

---
+++ src/stdoutclient.cpp
@@ -0,0 +1,34 @@
+/*! \file stdoutclient.cpp
+ *  \brief Passes events from the event queue to stdout.
+ *
+ *  Copyright 2013 Raymond Zandbergen (ray.zandbergen@gmail.com)
+ */
+#include <stdio.h>
+#include "queue.h"
+#include "error.h"
+
+//! \brief Main entry point, no parameters.
+int main()
+{
+    Queue m_eventRxQueue;
+    m_eventRxQueue.openRead();
+    Event event;
+    for (;;)
+    {
+        m_eventRxQueue.receive(event);
+        printf("%04x %02x %02x %02x %02x %02x %02x %02x %02x %02x %02x\n",
+            event.m_sequenceNumber,
+            event.m_metaMode,
+            event.m_currentTrack,
+            event.m_currentSection,
+            event.m_trackIdxWithinSet,
+            event.m_type,
+            event.m_deviceId,
+            event.m_part,
+            event.m_midi[0],
+            event.m_midi[1],
+            event.m_midi[2]);
+        fflush(stdout);
+    }
+    return 0;
+}
