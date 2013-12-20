0965271b3ae21dad683950269ec3dc4e8c37115a
Ray Zandbergen <ray.zandbergen@gmail.com>
2013-12-20 19:10:55 +0000

    Added a section canvas.
    git-svn-id: svn://localhost/home/ray/svnrepos/patcher@228 7c8a5990-47a6-46bc-89a8-0082861630d0

--- tkclient.tcl
+++ tkclient.tcl
@@ -35,6 +35,7 @@ proc ProcessSectionChange { newSection } {
     set n [ xmlget track $::State::currentTrack section max ]
     set i [ expr $::State::currentSection + 1 ]
     .currentFrame.section configure -text "section $i/$n $name"
+    Section::Refresh
 }
 
 proc ProcessPatcherEvent { line } {
@@ -50,7 +51,8 @@ proc ProcessPatcherEvent { line } {
 }
 
 # wait for first event
-set channel [ open "|./stdout_client -f" ]
+#set channel [ open "|./stdout_client -f" ]
+set channel [ open "|./stdout_client" ]
 set firstLine [ gets $channel ]
 foreach i $eventMembers { lappend globals State::$i }
 eval "scan \"$firstLine\" \"%x %x %x %x %x %x %x %x %x %x %x\" $globals"
@@ -65,9 +67,51 @@ proc GetPatcherEvent {channel} {
     }
 }
 
+namespace eval Section {
+    proc Create { } {
+        canvas .c -width 30c -height 5c
+        for {set i 0} {$i < 8} {incr i} {
+            set row [ expr ( $i % 4 ) * 30 + 30 ]
+            set col [ expr ( $i / 4 ) * 180 + 30 ]
+            lappend ::Section::numberId [ .c create text ${col}p ${row}p -text 0 ]
+            set col [ expr $col + 20 ]
+            lappend ::Section::channelId [ .c create text ${col}p ${row}p -text 0 ]
+            set col [ expr $col + 40 ]
+            lappend ::Section::nameId [ .c create text ${col}p ${row}p -text {<name>} ]
+        }
+        return .c
+    }
+    proc Refresh { } {
+        set nofParts [ xmlget track $::State::currentTrack section $::State::currentSection \
+                                   part max ]
+        for {set i 0} {$i < 8} {incr i} {
+            set id [ lindex $::Section::numberId $i ]
+            .c dchars $id 0 end
+            set id [ lindex $::Section::channelId $i ]
+            .c dchars $id 0 end
+            set id [ lindex $::Section::nameId $i ]
+            .c dchars $id 0 end
+            if { $i < $nofParts } {
+                set name [ xmlget track $::State::currentTrack section $::State::currentSection \
+                            part $i name ]
+                set channel [ xmlget track $::State::currentTrack section $::State::currentSection \
+                               part $i channel ]
+                set id [ lindex $::Section::numberId $i ]
+                set num [ expr $i + 1 ]
+                .c insert $id end $num
+                set id [ lindex $::Section::channelId $i ]
+                incr channel
+                .c insert $id end $channel
+                set id [ lindex $::Section::nameId $i ]
+                .c insert $id end $name
+            }
+        }
+    }
+}
+
 namespace eval Fantom {
     proc Create { } {
-        canvas .c -width 30c -height 10c
+        canvas .fantomCanvas -width 30c -height 10c
         for {set i 0} {$i < 16} {incr i} {
             set preset [ xmlget track $::State::currentTrack \
                          performance part $i preset ]
@@ -76,13 +120,13 @@ namespace eval Fantom {
             set row [ expr ( $i % 4 ) * 30 + 30 ]
             set col [ expr ( $i / 4 ) * 180 + 30 ]
             set num [ expr $i + 1 ]
-            lappend ::Fantom::numberId [ .c create text ${col}p ${row}p -text $num ]
+            lappend ::Fantom::numberId [ .fantomCanvas create text ${col}p ${row}p -text $num ]
             set col [ expr $col + 40 ]
-            lappend ::Fantom::presetId [ .c create text ${col}p ${row}p -text $preset ]
+            lappend ::Fantom::presetId [ .fantomCanvas create text ${col}p ${row}p -text $preset ]
             set col [ expr $col + 80 ]
-            lappend ::Fantom::nameId [ .c create text ${col}p ${row}p -text $name ]
+            lappend ::Fantom::nameId [ .fantomCanvas create text ${col}p ${row}p -text $name ]
         }
-        return .c
+        return .fantomCanvas
     }
     proc Refresh { } {
         for {set i 0} {$i < 16} {incr i} {
@@ -91,11 +135,11 @@ namespace eval Fantom {
             set name [ xmlget track $::State::currentTrack \
                        performance part $i patch name ]
             set id [ lindex $::Fantom::presetId $i ]
-            .c dchars $id 0 end
-            .c insert $id end $preset
+            .fantomCanvas dchars $id 0 end
+            .fantomCanvas insert $id end $preset
             set id [ lindex $::Fantom::nameId $i ]
-            .c dchars $id 0 end
-            .c insert $id end $name
+            .fantomCanvas dchars $id 0 end
+            .fantomCanvas insert $id end $name
         }
     }
 }
@@ -119,6 +163,7 @@ pack .currentFrame.track
 label .currentFrame.section -text {section y 1/1}
 pack .currentFrame.section
 pack [ Fantom::Create ]
+pack [ Section::Create ]
 text .eventLog -width 40 -height 4
 pack .eventLog
 
